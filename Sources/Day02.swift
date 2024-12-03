22cc45f3dff704bdcdf6730818cac8d03f1c915c
Aaron <aaron.kosovich@gmail.com>
2024-12-03 11:45:28 +1100

    Day2 Solution

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -12,7 +12,7 @@ struct Day02: AdventDay {
     var data: String
     
     // Split the input data into its component parts and convert to [[Int]].
-    var measurements: [[Int]] {
+    var reports: [[Int]] {
         let lines = data.split(separator: "\n")
         let numbers = lines.map { line in
             line.split(separator: " ").compactMap { Int($0) }
@@ -21,11 +21,42 @@ struct Day02: AdventDay {
     }
     
     func part1() -> Int {
-        return 0
+        reports.filter { isValidSequence($0) }.count
     }
     
     func part2() -> Int {
-        return 0
+        reports.filter { isValidAfterRemoval($0) }.count
+    }
+    
+    private func isValidSequence(_ numbers: [Int]) -> Bool {
+        let differences = zip(numbers, numbers.dropFirst())
+            .map { $1 - $0 }
+            
+        guard differences.allSatisfy({ (1...3).contains(abs($0)) }) else {
+            return false
+        }
+        
+        return differences.allSatisfy { $0 > 0 } ||
+               differences.allSatisfy { $0 < 0 }
+    }
+
+    private func isValidAfterRemoval(_ numbers: [Int]) -> Bool {
+        // Try removing each index and check if any resulting sequence is valid
+        return (0..<numbers.count).contains { indexToRemove in
+            let modifiedNumbers = numbers.enumerated()
+                .compactMap { index, value in
+                    index != indexToRemove ? value : nil
+                }
+                
+            let differences = zip(modifiedNumbers, modifiedNumbers.dropFirst())
+                .map { $1 - $0 }
+                
+            guard differences.allSatisfy({ (1...3).contains(abs($0)) }) else {
+                return false
+            }
+            
+            return differences.allSatisfy { $0 > 0 } ||
+                   differences.allSatisfy { $0 < 0 }
+        }
     }
 }
-
---
+++ Tests/Day02.swift
@@ -0,0 +1,155 @@
+import XCTest
+
+@testable import AdventOfCode
+
+final class Day02Tests: XCTestCase {
+    // Smoke test data provided in the challenge question
+    let testData = """
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
+    """
+    
+    // Smoke tests
+    
+    func testPart1() throws {
+        let challenge = Day02(data: testData)
+        XCTAssertEqual(challenge.part1(), 2)
+    }
+    
+    func testPart2() throws {
+        let challenge = Day02(data: testData)
+        XCTAssertEqual(challenge.part2(), 4)
+    }
+    
+    // Unit tests
+    
+    func testEmptyInput() {
+        let day = Day02(data: "")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 0)
+    }
+    
+    func testAscending() {
+        let day = Day02(data: "1 2 3 4 5")
+        XCTAssertEqual(day.part1(), 1)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testDescending() {
+        let day = Day02(data: "5 4 3 2 1")
+        XCTAssertEqual(day.part1(), 1)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testUnsafeDistance() {
+        let day = Day02(data: "1 2 6 7 8")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 0)
+    }
+    
+    func testLongReport() {
+        let day = Day02(data: "1 2 3 4 5 6 7 8 9")
+        XCTAssertEqual(day.part1(), 1)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testDoubleDigitsLongAscending() {
+        let day = Day02(data: "11 12 13 14 15 16 17 18 19")
+        XCTAssertEqual(day.part1(), 1)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testDoubleDigitsLongDescending() {
+        let day = Day02(data: "19 18 17 16 15 14 13 12 11")
+        XCTAssertEqual(day.part1(), 1)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testEqualValues() {
+        let day = Day02(data: "1 2 4 4 6")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testBadFirstValue() {
+        let day = Day02(data: "3 2 4 5 6")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+
+    func testRepeatingFinalValue() {
+        let day = Day02(data: "1 2 3 4 5 5")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testInvertFirst() {
+        let day = Day02(data: "5 1 2 3 4 5")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testInvertLast() {
+        let day = Day02(data: "1 2 3 4 5 4")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testQuickJump() {
+        let day = Day02(data: "1 6 7 8 9")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testAscendingTriangleWave() {
+        let day = Day02(data: "1 2 3 4 3")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testDescendingTriangleWave() {
+        let day = Day02(data: "9 8 7 6 7")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testRemoveThenRepeat() {
+        let day = Day02(data: "7 10 8 10 11")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testRemoveThenRepeatLong() {
+        let day = Day02(data: "29 28 27 25 26 25 22 20")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testRedditOne() {
+        let day = Day02(data: "7 10 8 10 11")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testRedditTwo() {
+        let day = Day02(data: "29 28 27 25 26 25 22 20")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testRedditThree() {
+        let day = Day02(data: "8 9 10 11")
+        XCTAssertEqual(day.part1(), 1)
+        XCTAssertEqual(day.part2(), 1)
+    }
+    
+    func testRedditFour() {
+        let day = Day02(data: "75 77 72 70 69")
+        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part2(), 1)
+    }
+}
